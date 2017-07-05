cb17e492e463c85dd44dfb4200a142ed8a4841a3
Jody Vanden <jody.vanden@gmail.com>
2017-07-05 05:07:22 +0200

    responsive rework

--- app/assets/javascripts/homepage-sport.js
+++ app/assets/javascripts/homepage-sport.js
@@ -11,6 +11,19 @@ $(document).ready(function() {
     } else {
       $(this).removeClass("scale-up");
     }
+  });
+
+    $(".buttons-sports-center input:checked").parent(".buttons-sports-center").addClass("scale-up");
+
+    $(".buttons-sports-center").on("change", function(e){
+    // Change active tab
+    // console.log($(this).find("input").is(':checked'));
+
+    if( $(this).find("input").is(':checked')) {
+      $(this).addClass("scale-up");
+    } else {
+      $(this).removeClass("scale-up");
+    }
     // Show target tab-content (use class="hidden")
   });
 });
